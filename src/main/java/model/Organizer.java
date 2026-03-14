89dc23898ccdd0fabfd8da312f3d102d17c1292c
FrancescoPioPellegrino <frpiopellegrino@icloud.com>
2026-03-14 19:40:01 +0100

    Classe Organizer

---
+++ src/main/java/model/Organizer.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.util.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Organizer extends User {
+    //Lista degli hackaton gestiti.
+    private final List<Hackathon> managedHackathons;
+
+
+    public Organizer(String username, String password) {
+        super(username, password);
+        this.managedHackathons = new ArrayList<>();
+    }
+    //Creazione di un hackathon.
+    public Hackathon createHackathon(String title, int maxTeamNumber, int minTeamNumber, int maxTeamSize, int minTeamSize,Location location) {
+            Hackathon newHackathon = new Hackathon(title, maxTeamNumber, minTeamNumber, maxTeamSize, minTeamSize, this, location);
+                managedHackathons.add(newHackathon);
+                return newHackathon;
+        }
+    //Apertura iscrizioni all'hackathon.
+    public void openHackathonSubscription(Hackathon hackathon) {
+                    hackathon.setRegistrationStatus(true);
+        }
+    //Chiusura iscrizioni
+    public void closeHackathonSubscription(Hackathon hackathon) {
+            hackathon.setRegistrationStatus(false);
+        }
+    // Avvio di un hackathon.
+    public void startHackathon(Hackathon hackathon) {
+                hackathon.setStartDate(LocalDate.now());
+        }
+
+    //Chiusura dell' hackathon.
+    public void endHackathon(Hackathon hackathon, ArrayList(Team)) {
+                hackathon.end(Team);
+    }
+    //Invito del giudice
+    public void inviteJudge(Hackathon hackathon, Judge judge) {
+                hackathon.addJudge(judge);
+            }
+
+
+
+
+
+
+
+
+}
+
