faa8498bb162db303efe645d0801008c83197be4
Davide-student <davidepauni@gmail.com>
2026-03-14 18:16:02 +0100

    Aggiunta metodi
    Aggiunto costruttore, metodi getter e setter. Aggiunto un attributo di tipo Hackathon che indica l'hackathon occupante la sede in un dato momento (Serve a determinare se la sede è occupata).

--- src/main/java/model/Location.java
+++ src/main/java/model/Location.java
@@ -7,4 +7,42 @@ public class Location
 	private String street[40];
 	private int CAP;
 	private int doorNumber;	//Indica l'interno in un edificio
+    private Hackathon currentHackathon; //Questo attributo viene valorizzato mentre la sede è occupata per un                                                                 //hackathon. Quando la sede non è più occupata, viene valorizzato null e 
+                                        //mostrato nella lista delle sedi disponibili agli organizzatori.
+
+    //Costruttore per istanze di "Location"
+    public Location(String address, String street, int CAP, int doorNumber) 
+    {
+        this.address = address;
+        this.street = street;
+        this.CAP = CAP;
+        this.doorNumber = doorNumber;
+        this.currentHackathon = null;
+    }
+    //getter e setter
+    public void setCurrentHackathon(Hackathon currentHackathon) //Quando la sede viene occupata, "currentHackathon" viene valorizzato.
+    {  
+        this.currentHackathon = currentHackathon;
+    }
+    public String getAddress()
+    {
+        return this.address;
+    }
+    public String getStreet()
+    {
+        return this.street;
+    }
+    public int getCAP()
+    {
+        return this.CAP;
+    }
+    public int doorNumber()
+    {
+        return this.doorNumber;
+    }
+    public Hackathon getcurrentHackathon()
+    {
+        return this.currentHackathon;
+    }
+    
 }
