5c09afe8e3d67693d3ebcdce8adefcb97d0c23d7
Davide-student <davidepauni@gmail.com>
2026-03-18 17:36:48 +0100

    Update Organizer.java

--- src/main/java/model/Organizer.java
+++ src/main/java/model/Organizer.java
@@ -33,8 +33,8 @@ public class Organizer extends User {
     }
 
     //Chiusura dell' hackathon.
-    public void endHackathon(Hackathon hackathon, ArrayList(Team)) {
-                hackathon.end(Team);
+    public void endHackathon(Hackathon hackathon, ArrayList<Team> team) {
+                hackathon.end(team);
     }
     //Invito del giudice
     public void inviteJudge(Hackathon hackathon, Judge judge) {
