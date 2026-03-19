4507dfbdc51fb0f473af527cfbb95c259c00abe3
FrancescoPioPellegrino <frpiopellegrino@icloud.com>
2026-03-19 12:03:18 +0100

    Creazione classe Participant
    Classe Participant creata c'è da aggiungere la gestione degli inviti.

---
+++ src/main/java/model/Participant.java
@@ -0,0 +1,29 @@
+package model;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Participant extends User {
+    public Participant(String username, String password) {
+        super(username, password);
+        //Creazione team.
+        public void createHackathonTeam (String teamName, Hackathon hackathon){
+            Team newTeam = new Team(teamName, hackathon);
+            newTeam.addMember(this);
+        }
+   /*
+    public void sendInviteToParticipant(Participant){
+
+    }
+    public void receiveInvite(Participant ) {
+
+    }
+    public void manageTeamInvite() {
+    }
+    */
+
+        public void publishTeamUpdate (Team team, Update update){
+            team.addUpdate(update);
+
+        }
+    }
+}
