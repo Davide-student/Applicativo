7a23f8909955ed95df098df6636a5b88a0846531
FrancescoPioPellegrino <frpiopellegrino@icloud.com>
2026-03-19 12:07:34 +0100

    Aggiornamento.
    Modifica di alcuni errori.

--- src/main/java/model/Participant.java
+++ src/main/java/model/Participant.java
@@ -5,11 +5,13 @@ import java.util.List;
 public class Participant extends User {
     public Participant(String username, String password) {
         super(username, password);
-        //Creazione team.
-        public void createHackathonTeam (String teamName, Hackathon hackathon){
-            Team newTeam = new Team(teamName, hackathon);
-            newTeam.addMember(this);
+
         }
+        //Creazione team.
+    public void createHackathonTeam(String teamName) {
+        Team newTeam = new Team(teamName);
+        newTeam.addMember(this);
+    }
    /*
     public void sendInviteToParticipant(Participant){
 
