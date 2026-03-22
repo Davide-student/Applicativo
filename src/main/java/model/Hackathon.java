8533cbe333d00192112958936b55dc352e8c9069
Davide-student <davidepauni@gmail.com>
2026-03-23 00:42:51 +0100

    Modifica indentazione

--- src/main/java/model/Hackathon.java
+++ src/main/java/model/Hackathon.java
@@ -22,77 +22,78 @@ public class Hackathon
 	private Organizer eventOrganizer;	//Indica l'organizzatore dell'hackathon
 	private Location location; //Sede fisica in cui si tiene l'hackathon
     private TreeMap<Integer, String> scores;  //Contiene i nomi dei team ed i relativi punteggi, ordinati secondo i punteggi (Ossia le chiavi)
-}
 
-public Hackathon(String title, int maxTeamNumber, int minTeamNumber, int maxTeamSize, int minTeamSize, Organizer eventOrganizer, Location location)
-{
-        this.title = title;
-        this.maxTeamNumber = maxTeamNumber;
-        this.minTeamNumber = minTeamNumber;
-        this.maxTeamSize = maxTeamSize;
-        this.minTeamSize = minTeamSize:
-        this.registrationDate = null;
-        this.eventOrganizer = eventOrganizer;
-        this.location = location;
-        this.startDate = null;
-        this.endDate = null;
-        this.judgeslist = new ArrayList<Judges>();
-        this.registrationStatus = false;    //Quando l'hackathon viene creato, le registrazioni sono chiuse
-        this.scores = new TreeMap<Integer, String>();
-}
 
-//Metodi setter e getter necessari
-
-public void setStartDate(Date startDate)
-{
-    this.startDate = startDate;
-}
-
-public void setEndDate(Date endDate)
-{
-    this.endDate = endDate;
-}
-
-public void setDescription(File problemDescription)
-{
-    this.problemDescription = problemDescription;
-}
-
-public void setRegistrationStatus(boolean status)
-{
-    this.registrationStatus = status;
-}
-
-/*public TreeMap<Integer, String> getScores() //restituisce la classifica di fine hackathon.
-{
-    return this.scores;
-}*/
-
-//Metodi della classe
-
-public void addJudge(Judge j)
-{
-    this.judgesList.add(j);
-}
-
-public void addScoreForTeam(Team team)
-{
-    this.scores.put(team.score, team.title);  
-                                    //Il team viene aggiunto alla lista di punteggi, quando poi il giudice 
-                                    //termina l'hackathon, questa lista
-                                    //viene ordinata rispetto ai punteggi assegnati    
-}
-
-//Questo metodo viene richiamato dal metodo in organizer "endHackathon" quando questo termina l'Hackathon.
-public void end(ArrayList<Team> teams) //Alla fine dell'hackathon, viene definita la classifica dei team
-{
-    for(Team t: teams)
-    {
-        this.addScoreForTeam(t);    //Aggiunge il team alla classifica (TreeMap). "this" indica l'istanza                                            //corrente di classe(L'hackathon)
-    }
-
-    this.endDate = LocalDate.now();
-    
+	public Hackathon(String title, int maxTeamNumber, int minTeamNumber, int maxTeamSize, int minTeamSize, Organizer eventOrganizer, Location location)
+	{
+	        this.title = title;
+	        this.maxTeamNumber = maxTeamNumber;
+	        this.minTeamNumber = minTeamNumber;
+	        this.maxTeamSize = maxTeamSize;
+	        this.minTeamSize = minTeamSize:
+	        this.registrationDate = null;
+	        this.eventOrganizer = eventOrganizer;
+	        this.location = location;
+	        this.startDate = null;
+	        this.endDate = null;
+	        this.judgeslist = new ArrayList<Judges>();
+	        this.registrationStatus = false;    //Quando l'hackathon viene creato, le registrazioni sono chiuse
+	        this.scores = new TreeMap<Integer, String>();
+	}
+	
+	//Metodi setter e getter necessari
+	
+	public void setStartDate(Date startDate)
+	{
+	    this.startDate = startDate;
+	}
+	
+	public void setEndDate(Date endDate)
+	{
+	    this.endDate = endDate;
+	}
+	
+	public void setDescription(File problemDescription)
+	{
+	    this.problemDescription = problemDescription;
+	}
+	
+	public void setRegistrationStatus(boolean status)
+	{
+	    this.registrationStatus = status;
+	}
+	
+	/*public TreeMap<Integer, String> getScores() //restituisce la classifica di fine hackathon.
+	{
+	    return this.scores;
+	}*/
+	
+	//Metodi della classe
+	
+	public void addJudge(Judge j)
+	{
+	    this.judgesList.add(j);
+	}
+	
+	public void addScoreForTeam(Team team)
+	{
+	    this.scores.put(team.score, team.title);  
+	                                    //Il team viene aggiunto alla lista di punteggi, quando poi il giudice 
+	                                    //termina l'hackathon, questa lista
+	                                    //viene ordinata rispetto ai punteggi assegnati    
+	}
+	
+	//Questo metodo viene richiamato dal metodo in organizer "endHackathon" quando questo termina l'Hackathon.
+	public void end(ArrayList<Team> teams) //Alla fine dell'hackathon, viene definita la classifica dei team
+	{
+	    for(Team t: teams)
+	    {
+	        this.addScoreForTeam(t);    //Aggiunge il team alla classifica (TreeMap). "this" indica l'istanza                                            //corrente di classe(L'hackathon)
+	    }
+	
+	    this.endDate = LocalDate.now();
+	    
+	}
 }
 
 
